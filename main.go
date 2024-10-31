a34b40c0b1229e6d6b10acce668a9a99cae7b288
keepprogress <keepprogress@gmail.com>
2024-10-31 14:52:47 +0800

    update time per seconds

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container" // Import container package
-	"fyne.io/fyne/v2/widget"    // Import widget package
+	"time"
+
+	"fyne.io/fyne/v2/app"    // Import container package
+	"fyne.io/fyne/v2/widget" // Import widget package
 )
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("The time is : 03:04:05")
+	clock.SetText(formatted)
+}
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Clock")
 
-	w.SetContent(container.NewVBox(widget.NewLabel("Hello World!")))
+	clock := widget.NewLabel("")
+	updateTime(clock)
+
+	w.SetContent(clock)
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
 	w.ShowAndRun()
 }
